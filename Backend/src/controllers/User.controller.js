51dd2c15720ef1101331ffba64f921c11dc08a12
Abhayraj B <abhayraj27052005@gmail.com>
2025-08-31 02:11:16 +0530

    the roles and storage updation is done

--- Backend/src/controllers/User.controller.js
+++ Backend/src/controllers/User.controller.js
@@ -7,22 +7,34 @@ const supabase = connectSupabase()
      
      try {
          
-         const {email,password}=req.body;
+         const {email,password,full_name}=req.body;
 
-         if(!email || !password){
+         if(!email || !password || !full_name){
 
-            return res.status(400).json({error:"Email and password is missing"});
+            return res.status(400).json({error:"Email or password or full_name is missing"});
          }
 
         const {data,error}=await supabase.auth.signUp({
             email,
-            password
+            password,
+            full_name
         })
 
         if(error){
-
+            
             return res.status(400).json({error:error.message})
         }
+        // inserting into the tables 
+
+        const {error:dbError}=await supabase.from("users").insert([{
+            id:data.user.id,
+            email:data.user.email,
+            full_name:full_name,
+            role:"student"
+        }])
+        
+        if(dbError) return res.status(500).json({error:dbError.message})
+            
 
         return res.status(200).json({
             message:"user signed up successfully",
